5f0829bf6a0d0c2f43193c537c230587418a454a
bhyn <bhyn9785@gmail.com>
2025-03-06 13:28:07 +0900

    initial setting

---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/bhyn/Documents/BooksRoom/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/bhyn/Documents/BooksRoom/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/bhyn/Documents/BooksRoom/ios/build/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap
