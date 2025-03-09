308e288b429d716d1aa4cb9b8f70c627229a79fb
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-09 19:08:26 +0900

    Feat: 상세화면&리뷰화면 별점 연동

--- lib/screens/book_detail_screen.dart
+++ lib/screens/book_detail_screen.dart
@@ -24,6 +24,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
   bool isWishing = false;
   bool isReading = false;
   bool isReviewed = false;
+  int starRating = 0;
   bool isDataLoaded = false; // 데이터가 이미 로드되었는지 여부 체크
 
   // Firebase 서비스 인스턴스
@@ -69,9 +70,10 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
           isWishing = savedBook.isWishing;
           isReading = savedBook.isReading;
           isReviewed = savedBook.isReviewed;
+          starRating = savedBook.starRating ?? 0;
         });
         print(
-          '저장된 책 정보: 찜=${savedBook.isWishing}, 읽는중=${savedBook.isReading}, 리뷰=${savedBook.isReviewed}',
+          '저장된 책 정보: 찜=${savedBook.isWishing}, 읽는중=${savedBook.isReading}, 리뷰=${savedBook.isReviewed}, 별점=${savedBook.starRating}',
         );
       } else {
         // 저장된 책이 없으면 API에서 가져온 정보를 Firebase에 저장
@@ -109,6 +111,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
     required bool isWishing,
     required bool isReading,
     required bool isReviewed,
+    required int? starRating,
   }) async {
     if (_savedBookModel == null) {
       print('저장된 책 모델이 없어 상태를 업데이트할 수 없습니다');
@@ -125,6 +128,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
         isWishing: isWishing,
         isReading: isReading,
         isReviewed: isReviewed,
+        starRating: starRating,
       );
 
       await _bookFirebaseService.saveBook(updatedModel);
@@ -269,20 +273,12 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
                         Expanded(flex: 4, child: Image.network(cover)),
                       ],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        for (int i = 0; i < 5; i++)
-                          Icon(
-                            Icons.star_outline_outlined,
-                            color: GRAY200_LINE,
-                            size: 50,
-                          ),
-                      ],
-                    ),
 
+                    // 별점 섹션
+                    buildStarRating(),
                     SizedBox(height: 20),
+
+                    // 작품 정보 섹션
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
@@ -328,6 +324,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
                                     isWishing: isWishing,
                                     isReading: isReading,
                                     isReviewed: isReviewed,
+                                    starRating: starRating,
                                   );
                                 },
                       ),
@@ -358,6 +355,10 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
                                 ? null
                                 : () {
                                   setState(() {
+                                    // 읽기 시작할 때(false->true) isWishing을 false로 설정
+                                    if (!isReading) {
+                                      isWishing = false;
+                                    }
                                     isReading = !isReading;
                                   });
                                   // firebase 업데이트
@@ -365,6 +366,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
                                     isWishing: isWishing,
                                     isReading: isReading,
                                     isReviewed: isReviewed,
+                                    starRating: starRating,
                                   );
                                 },
                       ),
@@ -401,6 +403,7 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
                                       isWishing: isWishing,
                                       isReading: isReading,
                                       isReviewed: isReviewed,
+                                      starRating: starRating,
                                     ).then((_) {
                                       // 그 후 리뷰 화면으로 이동
                                       _navigateToReviewScreen();
@@ -449,6 +452,36 @@ class _BookDetailScreenState extends State<BookDetailScreen> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget buildStarRating() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: List.generate(5, (index) {
+        return IconButton(
+          icon: Icon(
+            index < starRating ? Icons.star : Icons.star_border_outlined,
+            color: index < starRating ? MAIN_COLOR : GRAY200_LINE,
+            size: 50,
+          ),
+          onPressed: () {
+            setState(() {
+              starRating = index + 1;
+              isWishing = false;
+              isReading = false;
+            });
+
+            _updateBookStatus(
+              isWishing: isWishing,
+              isReading: isReading,
+              isReviewed: isReviewed,
+              starRating: starRating,
+            );
+          },
+        );
+      }),
     );
   }
 }
