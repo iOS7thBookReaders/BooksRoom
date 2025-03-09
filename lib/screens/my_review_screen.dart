4e90262e081740e91745805988f1f23124ec6f50
bhyn <bhyn9785@gmail.com>
2025-03-09 17:21:46 +0900

    Feat: 독후감 목록 구현

--- lib/screens/my_review_screen.dart
+++ lib/screens/my_review_screen.dart
@@ -1,3 +1,4 @@
+import 'package:books_room/components/color.dart';
 import 'package:flutter/material.dart';
 
 import '../models/book_model.dart';
@@ -17,11 +18,67 @@ class MyReviewScreen extends StatelessWidget {
         elevation: 0,
         scrolledUnderElevation: 0,
       ),
-      body: ListView.builder(
-        itemCount: 0,
-        itemBuilder: (context, index) {
-          return null;
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.separated(
+          separatorBuilder: (context, index) {
+            return const Divider(height: 1, color: GRAY200_LINE);
+          },
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return _buildReviewCell(items[index]);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildReviewCell(BookModel item) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                item.title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                item.author,
+                style: const TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Text(
+                '${item.oneLineComment}',
+                style: const TextStyle(fontSize: 14),
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  for (int i = 0; i < 5; i++)
+                    if (i < item.starRating!)
+                      const Icon(Icons.star, color: MAIN_COLOR, size: 20)
+                    else
+                      const Icon(
+                        Icons.star_outline_outlined,
+                        color: GRAY300_DISABLE,
+                        size: 20,
+                      ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
